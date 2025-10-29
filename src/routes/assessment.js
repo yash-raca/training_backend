adbe211624d89ff62312181173a3ed68f409b56e
yash-raca <mis@rachitagarwalassociates.com>
2025-10-29 16:09:21 +0530

    updated course module

--- src/routes/assessment.js
+++ src/routes/assessment.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const authenticateToken = require('../middleware/auth');
+const authorize = require('../middleware/authorize');
 
 const prisma = new PrismaClient();
 
